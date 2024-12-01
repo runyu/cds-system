87a80668ac8ace41511663954d1c4d4239aaa3ef
runyu <wangrunyu010@gmail.com>
2024-12-01 23:39:34 +0800

    init commit

---
+++ src/test/java/com/food/ordering/system/cdssystemlocal/CdsSystemLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.cdssystemlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdsSystemLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
